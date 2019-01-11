cb12bdacfa65d38ce9dcc5e7d119c3d9afdb9834
Anton Glezman <a.glezman@redmadrobot.com>
2019-01-11 12:37:09 +0300

    Initial Commit

---
+++ RMRShimmering/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RMRShimmering
+//
+//  Created by Anton Glezman on 11/01/2019.
+//  Copyright © 2019 RedMadRobot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
